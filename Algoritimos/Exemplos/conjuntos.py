431147810d8b8258fa3277585b700fc0c2569d6d
Jeihcio Francis <jeihcio.francis@hotmail.com>
2024-08-13 17:27:01 -0300

    Create conjuntos.py

---
+++ Algoritimos/Exemplos/conjuntos.py
@@ -0,0 +1,12 @@
+frutas = set(["abacate", "tomate", "banana"])
+vegetais = set(["beterraba", "cenoura", "tomate"])
+
+print("\nUnião")
+print(frutas | vegetais)
+
+print("\nIntersecção")
+print(frutas & vegetais)
+
+print("\nDiferença")
+print(frutas - vegetais)
+print(vegetais - frutas)
